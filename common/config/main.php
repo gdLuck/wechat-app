2d2ad22853d368641488f6280fa79c78bd98fc69
gdluck <gdluck256@g-mail.com>
2018-03-06 17:23:56 +0800

    add settings

--- common/config/main.php
+++ common/config/main.php
@@ -6,8 +6,29 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => require(__DIR__ . '/db.php'),
+
         'cache' => [
+            'class' => 'common\components\Cache',
+            'keyPrefix'   => 'yii:vrpeng:datacache:',
+            'redis' => [
+                'hostname' => '',
+                'password' => '',#本地配置会覆盖线上配置
+                'port' => 6379,
+                'database' => 0,
+            ]
+        ],
+        'file_cache' => [
             'class' => 'yii\caching\FileCache',
+            //'keyPrefix' => 'fileCache_vrpeng:',
+            'directoryLevel' => 2,
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => '',
+            'password' => '',#本地配置会覆盖线上配置
+            'port' => 6379,
+            'database' => 0,
         ],
     ],
 ];
