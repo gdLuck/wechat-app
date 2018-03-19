e60e8aa1bfb523dff137ee39c3077924c9ae04ca
gdluck <gdluck256@g-mail.com>
2018-03-19 12:18:51 +0800

    追加常量配置

--- api/web/index-prod.php
+++ api/web/index-prod.php
@@ -1,6 +1,9 @@
 <?php
 require(__DIR__ . '/../../common/config/constant-prod.php');
 
+// 定义使用情况 0正常 1初次验证
+define('FIRST_TIME', 1);
+
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
